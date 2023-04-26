e50656aed60c34bb98ae5361a518499b19ad5446
Ocupuc <777leon@list.ru>
2023-04-26 15:11:37 +0500

    НАчинаем

---
+++ src/test/java/ru/ocupuc/spring_jsf/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ocupuc.spring_jsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
